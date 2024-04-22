47cbb2bfdc26cb6704f63b71ab7d02fb89d129da
arthurcaraujo <arthur-lagamar@hotmail.com>
2024-04-22 11:43:10 -0300

    Add a header component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FooterComponent } from './components/footer/footer.component';
+import { HeaderComponent } from './components/header/header.component';
 import { MainComponent } from './components/main/main.component';
 import { RouterOutlet } from '@angular/router';
 
@@ -10,6 +11,7 @@ import { RouterOutlet } from '@angular/router';
     imports: [
         CommonModule,
         FooterComponent,
+        HeaderComponent,
         MainComponent,
         RouterOutlet
     ],
